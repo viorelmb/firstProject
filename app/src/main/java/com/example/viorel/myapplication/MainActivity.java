58341459eb91e04d541597bb238b59f25f831585
Viorel Zota <zota.v@mobilesoft.com>
2018-07-24 14:38:09 +0300

    feat: first commit

---
+++ app/src/main/java/com/example/viorel/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.viorel.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
